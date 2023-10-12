a4a03f09709f1725c7836b700dd89b40159a8bda
Jayam <jayampatel04@gmail.com>
2023-10-12 22:32:22 +0530

    added codeforces api

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import codeforces
 from leetcode import contest
 
 app = FastAPI()
@@ -14,21 +15,37 @@ app.add_middleware(
     allow_headers=["*"],  # Set this to the HTTP headers you want to allow
 )
 
+@DeprecationWarning
 @app.get("/{username}/leetcode/contest")
 def get_leetcode_info(username: str):
     return contest.get_contest_data(username)
 
+@DeprecationWarning
 @app.get("/{username}/leetcode/contest/basic")
 def get_leetcode_info(username: str):
     return contest.get_contest_basic(username)
 
+@DeprecationWarning
 @app.get("/{username}/leetcode/contest/basic/rank")
 def get_leetcode_rank(username: str):
     pass
     return contest.get_contest_rank(username)
 
+@DeprecationWarning
 @app.get("/{username}/leetcode/{query}")
 def get_leetcode_info_based_on_query(username: str, query: str):
     query = query.split("+")
     pass
     return contest.get_leetcode_info(username, query)
+
+@app.get("/codeforces/{username}")
+def get_codeforces_info(username: str):
+    return {
+        "rating": codeforces.get_current_rating(username)
+    }
+
+@app.get("/leetcode/{username}")
+def get_leetcode_info(username: str):
+    return {
+        "rating": contest.get_contest_basic(username)["data"]["userContestRanking"]["rating"]
+    }
