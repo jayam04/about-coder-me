112621e3224cb7f7b32744b19c24be4d64606cb8
Jayam <jayampatel04@gmail.com>
2023-10-14 11:34:00 +0530

    updated workflow

--- .github/scripts/update_status.py
+++ .github/scripts/update_status.py
@@ -1,16 +1,20 @@
 import json
 import requests
 import time
+import re
 
 base_url = "https://about-coderme.vercel.app/"
 websites = {
-    "leetcode": ["jayampatel"],
+    "leetcode": ["jayampatel", "hasan_15_07_03"],
     "codeforces": ["jayampatel"]
 }
 status_data = {}
 
+# Get the status of each website
 for website in websites:
     status_data[website] = []
+
+    # Get the status of each testcase for each website
     for testcase in websites[website]:
         endpoint = f"{base_url}{website}/{testcase}"
         response = requests.get(endpoint)
@@ -20,8 +24,22 @@ for website in websites:
             "status_code": response.status_code,
         })
 
+    # Update the status badge in the README.md file
+    with open("README.md", "r") as readme_file:
+        readme_content = readme_file.read()
+
+    avg_success = sum(1 for status in status_data[website] if status["status"] == "success") / len(status_data[website])
+    status = "UP"  if avg_success >= 0.5 else "DOWN"
+    color = "brightgreen" if avg_success > 0.7 else "red"
+    readme_content = re.sub(r"!\[" + website + r"\]\(.*\)", f"![{website}](https://img.shields.io/badge/{website}-success%20rate:%20{int(avg_success * 100)}-{color})", readme_content)
+
+    with open("README.md", "w") as readme_file:
+        readme_file.write(readme_content)
+
+
+# Create the status data
 status_data = {
-    "timestamp": time.time(),
+    "timestamp": int(time.time()),
     "data": status_data,
     "base_url": base_url
 }
--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 API to access your leetcode rankings.  
 
+![leetcode](https://img.shields.io/badge/leetcode-success%20rate:%20100-brightgreen)  
+![codeforces](https://img.shields.io/badge/codeforces-success%20rate:%20100-brightgreen)  
+
 ## Usage
 
-Make `GET` request at `https://about-coderme.vercel.app/{username}/leetcode/contest/` for your contest (leetcode) related data.  
-For more see [URL Formatting](url-format.md)  
+Make `GET` request at `https://about-coderme.vercel.app/{website}/{username}/` for all data for that website  
+For more see [URL Formatting](docs/endpoints.md)  
