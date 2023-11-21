fc2138488f6d321b033ab91531d95f194614217e
Jayam <jayampatel04@gmail.com>
2023-11-21 20:35:18 +0530

    added codechef ratings

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import platform_scripts.codeforces as codeforces
 from leetcode import contest
+from platform_scripts import codechef
 
 app = FastAPI()
 
@@ -17,22 +18,26 @@ app.add_middleware(
     allow_headers=["*"],  # Set this to the HTTP headers you want to allow
 )
 
+
 @DeprecationWarning
 @app.get("/{username}/leetcode/contest")
 def get_leetcode_info(username: str):
     if username[0] != '@':
         return contest.get_contest_data(username)
 
+
 @DeprecationWarning
 @app.get("/{username}/leetcode/contest/basic")
 def get_leetcode_info(username: str):
     return contest.get_contest_basic(username)
+
 
 @DeprecationWarning
 @app.get("/{username}/leetcode/contest/basic/rank")
 def get_leetcode_rank(username: str):
     pass
     return contest.get_contest_rank(username)
+
 
 @DeprecationWarning
 @app.get("/{username}/leetcode/{query}")
@@ -41,11 +46,13 @@ def get_leetcode_info_based_on_query(username: str, query: str):
     pass
     return contest.get_leetcode_info(username, query)
 
+
 @app.get("/codeforces/{username}")
 def get_codeforces_info(username: str):
     return {
         "rating": codeforces.get_current_rating(username)
     }
+
 
 @app.get("/leetcode/{username}")
 def get_leetcode_info(username: str):
@@ -53,9 +60,11 @@ def get_leetcode_info(username: str):
         "rating": contest.get_contest_basic(username)["data"]["userContestRanking"]["rating"]
     }
 
+
 @app.get("/status")
 def get_status():
     return json.load(open("status.json", "r"))
+
 
 @app.get("/{username}")
 def get_all_ratings_from_username(username: str):
@@ -72,5 +81,7 @@ def get_all_ratings_from_username(username: str):
         body['leetcode'] = get_leetcode_info(userdata['leetcode'])  # TODO: use better functions
     if "codeforces" in userdata.keys():
         body['codeforces'] = get_codeforces_info(userdata['codeforces'])  # TODO: use better functions
+    if "codechef" in userdata.keys():
+        body['codechef'] = codechef.get_contest_rating(userdata['codeforces'])  # TODO: function is ok but not complete
 
     return body
---
+++ platform_scripts/codechef.py
@@ -0,0 +1,11 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def get_contest_rating(username: str) -> int:
+    response = requests.get(f"https://www.codechef.com/users/{username}")
+    if response.ok:
+        soup = BeautifulSoup(response.text, features="html.parser")
+        rating = soup.find("div", class_="rating-number").text.split("?")[0]
+        return int(rating)
